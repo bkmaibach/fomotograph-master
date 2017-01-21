c1537c64310ddda4dc25eb83dc79555421e461bd
bmaib_000 <keith@maibach.ca>
2017-01-20 19:22:11 -0800

    Add initialize funciton

--- models/product.rb
+++ models/product.rb
@@ -4,12 +4,20 @@ require 'HTTParty'
 require 'json'
 
 class Product
-    url = 'https://fomotograph-api.udacity.com/products/json'
-    DATA = HTTParty.get(url)['photos']
+	url = 'https://fomotograph-api.udacity.com/products/json'
+	DATA = HTTParty.get(url)['photos']
 
-    def self.all
-    DATA.map { |product| new(product) }
-  end
+	def initialize(product_data = {})
+		@id = product_data['id']
+		@title = product_data['title']
+		@location = product_data['location']
+		@summary = product_data['summary']
+		@url = product_data['url']
+		@price = product_data['price']
+	end
+
+	def self.all
+		DATA.map { |product| new(product) }
+	end
 
 end
-
