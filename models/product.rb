e3bdfa9916aab1f3adb56356505abd5e9d5e9225
bmaib_000 <keith@maibach.ca>
2017-01-20 18:45:15 -0800

    Add the all method to Product class

--- models/product.rb
+++ models/product.rb
@@ -6,5 +6,10 @@ require 'json'
 class Product
     url = 'https://fomotograph-api.udacity.com/products/json'
     DATA = HTTParty.get(url)['photos']
+
+    def self.all
+    DATA.map { |product| new(product) }
+  end
+
 end
 
