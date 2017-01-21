b0b2c70fd86e748c9f88103582050fb8fc3123f1
bmaib_000 <keith@maibach.ca>
2017-01-20 20:15:04 -0800

    add sample funciton to product.rb

--- models/product.rb
+++ models/product.rb
@@ -5,8 +5,12 @@ require 'httparty'
 require 'json'
 
 class Product
+	attr_reader :id, :title, :location, :summary, :url, :price
+
 	url = 'https://fomotograph-api.udacity.com/products.json'
 	DATA = HTTParty.get(url)['photos']
+
+	LOCATIONS = ['canada', 'england', 'france', 'ireland', 'mexico', 'scotland', 'taiwan', 'us']
 
 	def initialize(product_data = {})
 		@id = product_data['id']
@@ -20,5 +24,15 @@ class Product
 	def self.all
 		DATA.map { |product| new(product) }
 	end
+
+	def self.sample_locations
+			@products = []
+
+		LOCATIONS.each do |location|
+  			@products.push self.all.select { |product| product.location == location }.sample
+  		end
+  		return @products
+	end
+
 
 end
